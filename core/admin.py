84d4071e70e4e70b59e42bb24a70be56932d6735
Alexchiper1 <alexchiper20@gmail.com>
2026-04-18 17:12:36 +0100

    Added employer, job, and application models

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from .models import CandidateProfile, Skill, CandidateSkill
+from .models import EmployerProfile, Job, Application
 
 admin.site.register(CandidateProfile)
 admin.site.register(Skill)
 admin.site.register(CandidateSkill)
+
+admin.site.register(EmployerProfile)
+admin.site.register(Job)
+admin.site.register(Application)
--- core/models.py
+++ core/models.py
@@ -22,3 +22,30 @@ class CandidateSkill(models.Model):
 
     def __str__(self):
         return f"{self.candidate} - {self.skill}"
+    
+class EmployerProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    company_name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.company_name
+
+
+class Job(models.Model):
+    employer = models.ForeignKey(EmployerProfile, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    location = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
+
+
+class Application(models.Model):
+    candidate = models.ForeignKey(CandidateProfile, on_delete=models.CASCADE)
+    job = models.ForeignKey(Job, on_delete=models.CASCADE)
+    status = models.CharField(max_length=50, default="Pending")
+
+    def __str__(self):
+        return f"{self.candidate} -> {self.job}"
